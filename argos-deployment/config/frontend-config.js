db783a5aa0ffa0f8c67a7e668ada3d2a0838757b
Christian Friedow <christian@friedow.com>
2017-05-22 09:38:35 +0200

    Added frontend websocket

--- argos-deployment/config/frontend-config.js
+++ argos-deployment/config/frontend-config.js
@@ -2,6 +2,7 @@ const config = {
 	projectName: "Argos",
 	useBackendMock: false,
 	backendRESTRoute: "${HOST}/api",
+	backendWebSocketURL: "${WEBSOCKET_HOST}/api/notifications",
 	colors: {
 		primaryDark: "#000051",
 		primary: "#1a237e",
