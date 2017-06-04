9cd34843ffa5adb59170204d5c467278ff0b68e7
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-06-04 16:48:03 +0200

    Configure attributes

--- argos-deployment/config/attribute-config.js
+++ argos-deployment/config/attribute-config.js
@@ -3,6 +3,20 @@ This is the config for displaying the attributes. It must have the same EntityTy
 backend. Then, according to the requirements, the attributes, that should be displayed on the GridView are named and
 assigned the value 1 (integer).
  */
-const attributeConfig = {};
+const attributeConfig = {
+	"ProductFamily": {
+		"IntroductionDate": 1,
+		"NumberOfProductsInAction": 1
+	},
+	"Product": {
+		"Brand": 1,
+		"ProductId": 1,
+		"Description": 1
+	},
+	"Configuration": {
+		"CodingPlugId": 1,
+		"SoftwareVersion": 1
+	}
+};
 
 export default attributeConfig;
